f38ef0784b5c3e32bba10d6cf152c4b83639bdbf
7hil <phil.ph7@gmail.com>
2016-09-11 00:25:58 +0800

    first usable version

---
+++ README.md
@@ -0,0 +1,7 @@
+# mac_ear_control
+
+A simple Mac app to capture earphone button events and translate them to multimedia key event. Tested on Mac OS X EI Captain (10.11.6)
+
+- Earphone button event handling based on [DDHidLib](https://github.com/Daij-Djan/DDHidLib)
+- Multimedia key event emulation based on [Spotify Bluetooth Headset Listener](https://github.com/jguice/mac-bt-headset-fix)
+- Icon from [OpenClipart-Vectors-30363](https://pixabay.com/en/icon-headphones-hearing-157355/)
--- mac_ear_control/AppDelegate.m
+++ mac_ear_control/AppDelegate.m
@@ -7,20 +7,155 @@
 //
 
 #import "AppDelegate.h"
+#import "DDHidLib.h"
+#import "MediaKey.h"
+#import "IOKit/hid/IOHIDManager.h"
+#include <IOKit/usb/IOUSBLib.h>
+
 
 @interface AppDelegate ()
-
-@property (weak) IBOutlet NSWindow *window;
+@property (weak) IBOutlet NSMenu * statusItemMenu;
 @end
 
-@implementation AppDelegate
+@implementation AppDelegate {
+    NSStatusItem * statusItem;
+    DDHidAppleMikey * mCurrentMikey;
+}
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
+    [self setupMenuItem];
+    // unload rcd service to prevent iTunes show up
+    // when push the earphone button
+    [NSTask launchedTaskWithLaunchPath:@"/bin/launchctl" arguments:@[@"unload",@"/System/Library/LaunchAgents/com.apple.rcd.plist"]];
+
+    // from http://stackoverflow.com/questions/10843559/cocoa-detecting-usb-devices-by-vendor-id/24832676#24832676
+    // from http://stackoverflow.com/questions/27963870/mac-os-x-usb-hid-how-the-receive-device-added-device-removed-callbacks
+    IOHIDManagerRef HIDManager = IOHIDManagerCreate(kCFAllocatorDefault,
+                                                    kIOHIDOptionsTypeNone);
+    IOHIDManagerSetDeviceMatching(HIDManager, nil);  // match all device
+    // Here we use the same callback for insertion & removal.
+    IOHIDManagerRegisterDeviceMatchingCallback(HIDManager, Handle_UsbDetectionCallback, (__bridge void*)self);
+    IOHIDManagerRegisterDeviceRemovalCallback(HIDManager, Handle_UsbDetectionCallback2, (__bridge void*)self);
+    IOHIDManagerScheduleWithRunLoop(HIDManager, CFRunLoopGetMain(), kCFRunLoopDefaultMode);
+    IOReturn IOReturn = IOHIDManagerOpen(HIDManager, kIOHIDOptionsTypeNone);
+    if(IOReturn) puts("IOHIDManagerOpen failed.");
+    [self toggleDeckIcon:true];
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
-    // Insert code here to tear down your application
+    [NSTask launchedTaskWithLaunchPath:@"/bin/launchctl" arguments:@[@"load",@"/System/Library/LaunchAgents/com.apple.rcd.plist"]];
+}
+
+// New device has been added (callback function)
+static void Handle_UsbDetectionCallback(void *inContext, IOReturn inResult, void *inSender, IOHIDDeviceRef inIOHIDDeviceRef) {
+    NSLog(@"device added: %p",
+          (void *)inIOHIDDeviceRef);
+    [(__bridge AppDelegate *)(inContext) updateMikey];
+    [(__bridge AppDelegate *)(inContext) updateMikey]; // run twice in case of excption when earphone removed
+}
+
+// A device has been removed (callback function)
+static void Handle_UsbDetectionCallback2(void *inContext, IOReturn inResult, void *inSender, IOHIDDeviceRef inIOHIDDeviceRef) {
+    NSLog(@"device removed: %p",
+          (void *)inIOHIDDeviceRef);
+    [(__bridge AppDelegate *)(inContext) updateMikey];
+}
+
+- (void) setupMenuItem {
+    if(statusItem == nil) {
+        statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSSquareStatusItemLength];
+        statusItem.image = [NSImage imageNamed:@"MenuItemTemplate"];
+        statusItem.menu = _statusItemMenu;
+    }
+}
+
+- (bool) updateMikey {
+    @try {
+        NSArray * mikeys = [DDHidAppleMikey allMikeys] ;
+        
+        [mikeys makeObjectsPerformSelector: @selector(setDelegate:)
+                                withObject: self];
+        [self setMikey: [mikeys firstObject]];
+    }
+    @catch (NSException *e) {
+        NSLog(@"%@", e.description);
+        NSLog(@"error!");
+        return false;
+    }
+    return true;
+}
+
+- (void) setMikey: (DDHidAppleMikey *) newMikey
+{
+    if (newMikey != nil) {
+        if (mCurrentMikey != nil)
+        {
+            [mCurrentMikey stopListening];
+        }
+        mCurrentMikey = newMikey;
+        [mCurrentMikey startListening];
+    } else {
+        [mCurrentMikey stopListening];
+    }
+}
+
+- (void) toggleDeckIcon : (bool) disable {
+    // from http://stackoverflow.com/a/9220857/1813988
+    if (disable) {
+        [NSApp setActivationPolicy: NSApplicationActivationPolicyProhibited];
+    } else {
+        [NSApp setActivationPolicy: NSApplicationActivationPolicyAccessory];
+    }
+}
+
+- (IBAction) showDeckIcon:(id)sender {
+    NSLog(@"showDeckIcon");
+    [self toggleDeckIcon:false];
+}
+
+- (IBAction) hideDeckIcon:(id)sender {
+    NSLog(@"hideDeckIcon");
+    [self toggleDeckIcon:true];
+}
+
+@end
+
+@implementation AppDelegate (DDHidAppleMikeyDelegate)
+
+- (void) ddhidAppleMikey:(DDHidAppleMikey *)mikey press:(unsigned)usageId upOrDown:(BOOL)upOrDown
+{
+    if (upOrDown == TRUE) {
+#if DEBUG
+        NSLog(@"Apple Mikey keypress detected: %d", usageId);
+#endif
+        switch (usageId) {
+            case kHIDUsage_GD_SystemMenu:
+                NSLog(@"Play/Pause");
+                [MediaKey send:NX_KEYTYPE_PLAY];
+                break;
+            case kHIDUsage_GD_SystemMenuRight:
+                NSLog(@"Next");
+                // NX_KEYTYPE_NEXT seems invalid here
+                [MediaKey send:NX_KEYTYPE_FAST];
+                break;
+            case kHIDUsage_GD_SystemMenuLeft:
+                NSLog(@"Previous");
+                // NX_KEYTYPE_PREVIOUS seems invalid here
+                [MediaKey send:NX_KEYTYPE_REWIND];
+                break;
+            case kHIDUsage_GD_SystemMenuUp:
+                NSLog(@"sound up");
+                [MediaKey send:NX_KEYTYPE_SOUND_UP];
+                //[self sendMessagesToConnections:@selector(volumeUp)];
+                break;
+            case kHIDUsage_GD_SystemMenuDown:
+                NSLog(@"sound down");
+                [MediaKey send:NX_KEYTYPE_SOUND_DOWN];
+                break;
+            default:
+                NSLog(@"Unknown key press seen %d", usageId);
+        }
+    }
 }
 
 @end
